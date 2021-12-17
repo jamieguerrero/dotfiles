086e5aac8df9446e7a35edc6003be8ee57547105
Jamie Guerrero <Jamie.n.guerrero@gmail.com>
2021-12-16 21:58:34 -0500

    correct order of installing ohmyzsh and linking file

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ln -sf ~/.zsh-theme/random-emoji.zsh-theme ~/.oh-my-zsh/themes/random-emoji.zsh-theme
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'

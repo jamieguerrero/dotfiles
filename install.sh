7d295ff3b46690549debefc3cb8d5869d599fd86
Jamie Guerrero <Jamie.n.guerrero@gmail.com>
2021-12-16 21:46:02 -0500

    fix if statement and install oh-my-zsh by curl

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-ln -sf ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

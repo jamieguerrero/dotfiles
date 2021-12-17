bd4ea07192cf5fc28c0053a88afa9c377fe2681c
Jamie Guerrero <Jamie.n.guerrero@gmail.com>
2021-12-16 21:56:16 -0500

    add theme after installing oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/.zsh-theme/random-emoji.zsh-theme ~/.oh-my-zsh/themes/random-emoji.zsh-theme
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ln -sf ~/.zsh-theme/random-emoji.zsh-theme ~/.oh-my-zsh/themes/random-emoji.zsh-theme
+
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim --headless +PlugInstall +qall

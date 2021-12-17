eaccca6e8f9d1900522567b5bcd0a94b3393294c
Jamie Guerrero <Jamie.n.guerrero@gmail.com>
2021-12-16 21:48:45 -0500

    Add vim-plugin to install script

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim --headless +PlugInstall +qall
